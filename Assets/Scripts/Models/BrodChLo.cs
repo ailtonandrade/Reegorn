d3fcedd680055c2b0ced3d0a28d5d853cb459e23
Ailton Andrade <82040532+ailtonandrade@users.noreply.github.com>
2023-01-06 01:26:11 -0300

    update

--- Assets/Scripts/Models/BrodChLo.cs
+++ Assets/Scripts/Models/BrodChLo.cs
@@ -9,3 +9,10 @@ public class BrodChLo
     public string posZ {get;set;}
     public string rot {get;set;}
 }
+
+public static BrodChLo parse(Vector3 pos,float rot){
+this.posX = _pos.x.ToString("N3");
+this.posY = _pos.y.ToString("N3");
+this.posZ = _pos.z.ToString("N3");
+this.rot = _rot.ToString("N3"); 
+}
