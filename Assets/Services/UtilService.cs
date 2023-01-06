b664aa5de58f44a2e02d61031a6671aa60bf41cc
Ailton Andrade <82040532+ailtonandrade@users.noreply.github.com>
2023-01-06 08:30:49 -0300

    update

--- Assets/Services/UtilService.cs
+++ Assets/Services/UtilService.cs
@@ -35,6 +35,7 @@ public static void ShowLoading(string? detail)
     {
         GameObject.Find("HUD/HomeScreen/LoadingModal").gameObject.transform.localScale = new Vector3(1, 1, 0);
         //
+        //
         //implementar msg detail
     }
     
